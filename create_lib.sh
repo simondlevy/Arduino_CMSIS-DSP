b9ff932bb0bbbd5b5268f99f2225ff532d967370
simondlevy <simon.d.levy@gmail.com>
2025-04-23 03:10:31 -0400

    renamed:    create_lib -> create_lib.sh

--- create_lib.sh
+++ create_lib.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-mkdir src
-cp -r ${CMSIS_PATH}/CMSIS_5/CMSIS/DSP/* .
-cp -r Include/* src
-cp -r PrivateInclude/* src
-cp -r Source src
+git clone https://github.com/ARM-software/CMSIS-DSP
+cp -r CMSIS-DSP/Include/* src
+cp -r CMSIS-DSP/PrivateInclude/* src
+cp -r CMSIS-DSP/Source src
 find  src/Source/ -type d | sed 's|src/Source/||g' | xargs -I{} rm src/Source/{}/{}.c
 #Functions provided as .S for maximum speed
 rm src/Source/TransformFunctions/arm_bitreversal2.c
