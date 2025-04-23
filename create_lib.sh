5a4160bbf3b9aeb243c5d9f43dc8108f35367524
simondlevy <simon.d.levy@gmail.com>
2025-04-23 04:01:55 -0400

    modified:   create_lib.sh

--- create_lib.sh
+++ create_lib.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-git clone https://github.com/ARM-software/CMSIS-DSP
-cp -r CMSIS-DSP/Include/* src
-cp -r CMSIS-DSP/PrivateInclude/* src
-cp -r CMSIS-DSP/Source src
+CMSIS=${HOME}/.arduino15/packages/STMicroelectronics/tools/CMSIS/5.9.0/CMSIS/
+
+cp -r ${CMSIS}/DSP/Include/* src
+cp -r ${CMSIS}/DSP/PrivateInclude/* src
+cp -r ${CMSIS}/DSP/Source src
 find  src/Source/ -type d | sed 's|src/Source/||g' | xargs -I{} rm src/Source/{}/{}.c
 #Functions provided as .S for maximum speed
 rm src/Source/TransformFunctions/arm_bitreversal2.c
+echo You should now remove ${CMSIS}/DSP
